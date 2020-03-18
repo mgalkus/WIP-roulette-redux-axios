4f0a734f2930419125ddae6fff39229131d190f3
mgalkus <martynasgalkus@hotmail.com>
2020-03-18 18:21:19 +0200

    remove breaking GET_RESULT to have functioning next game rendering

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,15 +1,24 @@
 import update from "immutability-helper";
 
-export function getNextGameReducer(state = [], action) {
+export function getNextGameReducer(
+  state = { loadginMessage: "", list: [] },
+  action
+) {
   if (action.type === "NEXT_GAME") {
     // If state is empty (on page load):
-    if (state.length === 0) {
+    if (state.list.length === 0) {
       return [
         ...state,
         {
-          id: action.payload.id,
-          timeLeft: action.payload.fakeStartDelta,
-          result: null
+          list: {
+            $push: [
+              {
+                id: action.payload.id,
+                timeLeft: action.payload.fakeStartDelta,
+                result: null
+              }
+            ]
+          }
         }
       ];
     }
@@ -17,38 +26,42 @@ export function getNextGameReducer(state = [], action) {
     else {
       // And if new API call returns same id as previous (means we're still counting):
       if (
-        action.actionState[action.actionState.length - 1].id ===
+        action.actionState.list[action.actionState.list.length - 1].id ===
         action.payload.id
       ) {
         // Replacing last object in state with info from new API call:
-        if (action.payload.fakeStartDelta === 0) {
+        if (action.payload.fakeStartDelta <= 0) {
           return update(state, {
-            $splice: [
-              [
-                -1,
-                1,
-                {
-                  id: action.payload.id,
-                  timeLeft: action.payload.fakeStartDelta,
-                  loadingMessage: "Loading...",
-                  result: null
-                }
+            list: {
+              $splice: [
+                [
+                  -1,
+                  1,
+                  {
+                    id: action.payload.id,
+                    timeLeft: action.payload.fakeStartDelta,
+                    loadingMessage: "Loading...",
+                    result: null
+                  }
+                ]
               ]
-            ]
+            }
           });
         } else
           return update(state, {
-            $splice: [
-              [
-                -1,
-                1,
-                {
-                  id: action.payload.id,
-                  timeLeft: action.payload.fakeStartDelta,
-                  result: null
-                }
+            list: {
+              $splice: [
+                [
+                  -1,
+                  1,
+                  {
+                    id: action.payload.id,
+                    timeLeft: action.payload.fakeStartDelta,
+                    result: null
+                  }
+                ]
               ]
-            ]
+            }
           });
       }
       // If new API call returns info about new game:
